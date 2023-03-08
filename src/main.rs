d17612cfed550f26f9d642974b8a8c1422b4b762
goodvibrations32 <goodvibrations32@protonmail.com>
2023-03-09 01:48:39 +0200

    dynamic plots with minimum information

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,56 @@
 use itertools::Itertools;
-extern crate tdms;
 use std::path::Path;
+
+extern crate tdms;
 use tdms::data_type::TdmsDataType;
 use tdms::TDMSFile;
 extern crate itertools;
 extern crate itertools_num;
 use itertools_num::linspace;
-// use vector2d::Vector2D;
 use gnuplot::{Figure, Caption, Color,
               AxesCommon,LineWidth};
 
-struct WindSupply {
-    type_of_experiment: String,
-}
-struct SysFolder {
-    sys_path: String,
-    data_parent: String,
-    recording: String,
-}
-struct InnerExpFolders {
-    ca: String,
-    inv: String,
+// personal make path function
+mod path;
+#[derive(Debug)]
+struct Signal <'a> {
+    data: TDMSFile<'a>,
+    state: String,
+    inv_state_exp: String,
+    ws: String,
+
 }
 
-// windsupply::Compressed(String::from("compressed air"));
+//
 fn main(){
 
     // be sure of what to plot
     let system_path = String::from("/home/dtos_experiment/Documents/");
-    // FIXME goes out of bounds and plots 4 figures
-    // ONLY when using inverter's folder
-    let experiment = String::from("compressed air/");
-    // let experiment = String::from("inverter/");
-    let inverter_state = String::from("0");
-    let wind_speed = String::from("0");
+    let experiment = String::from("inverter/");
+    let inverter_state = String::from("1");
+    let wind_speed = String::from("5");
 
     // make the path
-    let sig_path = make_the_path(&system_path,
-                                 &experiment,
-                                 &inverter_state,
-                                 &wind_speed);
+    let sig_path = path::make_the_path(&system_path,
+                                       &experiment,
+                                       &inverter_state,
+                                       &wind_speed);
     // open a single file and store it in "file"
-    println!("{:?}", sig_path);
+
+    let sys_path = if Path::new(&sig_path).is_dir() {
+       let sp = String::from("D:/_data/WEL/WEL20220512/");
+        let papadakis_sys = &sp;
+        path::make_the_path(papadakis_sys,
+                            &experiment,
+                            &inverter_state,
+                            &wind_speed)
+
+    }else{String::from(sig_path)};
+    println!("{:?}", sys_path);
+
+
     let sig = match TDMSFile::from_path(
-        Path::new(&sig_path)
+        Path::new(&sys_path)
     ) {
         // catch the error
         // dont know exactly
@@ -51,101 +58,84 @@ fn main(){
         Ok(f) =>f,
         Err(e) => panic!("{:?}", e),
     };
-    let big_title = &experiment.replace("c","C").replace("/", "");
-    // let big_title = &experiment.replace("i","I").replace("/", "");
-    plot_time_domain(sig, &big_title);
+    // println!("{:?}", sig.segments.len());
+
+    let raw_signal = Signal{
+        data: sig,
+        state: experiment,
+        inv_state_exp: inverter_state,
+        ws: wind_speed,
+    };
+    raw_signal.plot_signal_in_time_domain("Wind2", &true)
 }
 
-pub fn make_the_path (local: &str, exp_type: &str, inv: &str, ws: &str) -> String {
-    // wind supply method
-    let experiment = WindSupply {
-        type_of_experiment: String::from(exp_type),
-        // compressed: String::from("compressed air/"),
-        // wind_tunnel: String::from("inverter/"),
-    };
 
-    // system folders such as pc folder, parent of data
-    // and recordings
-    let sys_folder = SysFolder {
-        sys_path: String::from(local),
-        data_parent: String::from("data_folder/"),
-        recording: String::from("measurements_12_05_22/new_record_prop_channel/"),
-    };
+impl Signal<'_> {
 
-    //rename to better stuff but for now i have only one method experiment
-    let inverter_and_ws = InnerExpFolders {
-        ca: String::from(format!("ca{inv}_{ws}.1/")),
-        inv: String::from(format!("in0_0.1/")),
-    };
+    fn plot_signal_in_time_domain (&self,
+                             witch_channel: &str,
+                             // plot_title: &str,
+                             draw: &bool) -> () {
+    let groups = self.data.groups();
+    // let single_chann =(self.channels("Wind Measurement")).get(witch_channel);
+    for group in &groups {
+        let channels = self.data.channels(&group);
+        // let total = &self.get_channel(ChannelPath("Wind2")); // .get(&witch_channel);
+        // let channel_single = &self;
 
-    let local_place = sys_folder.sys_path;
-    let parent = sys_folder.data_parent;
-    let record = sys_folder.recording;
-    let experiment_type = experiment.type_of_experiment;
-    // let experiment_state =
-    //     if experiment_type == "compressed air/" {
-    //         let experiment_state = inverter_and_ws.ca;
-    //     }else {
-    //         let experiment_state = inverter_and_ws.inv;
-    //     };
-
-    let comp = inverter_and_ws.ca;
-    // FIXME
-    // let strng = inverter_and_ws.inv;
-    // FIXME
-    const FILE_NAME: &str = "Data.tdms";
-    let full_path = format!(
-        "{local_place}{parent}{record}{experiment_type}{comp}{FILE_NAME}");
-    full_path
-
-}
-
-pub fn plot_time_domain <'a> (file_name: TDMSFile, plot_title: &str) {
-    let groups =file_name.groups();
-    for group in groups {
-        let channels = file_name.channels(&group);
+        // println!("{:?}", &channels.path); //("Wind Measurements")
+        // println!("{:?}", &channel_single.get_many_mut([2,3])); //("Wind Measurements")
         let mut _i = 0;
         for (_, channel) in channels{
-            let full_channel = match channel.data_type {
-                TdmsDataType::DoubleFloat(_) => file_name.channel_data_double_float(channel),
+            let full_group = match channel.data_type {
+                TdmsDataType::DoubleFloat(_) => self.data.channel_data_double_float(channel),
                 _ => {
                     panic!("{}", "channel for data type unimplemented")
                 }
             };
 
-            let full_channel_iterator = match full_channel {
+            let full_group_iterator = match full_group {
                 Ok(i) => i,
                 Err(e) => {
                     panic!("{:?}", e)
                 }
             };
-            let signal_raw: Vec<f64> = full_channel_iterator
-                .map_into::<f64>()
-                .collect();
+            let time_output: _ = full_group_iterator.map_into::<f64>().collect_vec();
+            println!("length of time signal = {:1?} time name = {:2?} channel name ={:3?}",
+                     &time_output.len(), &channel.group_path, &channel.path);
+            _i += 1;
 
             // make the time increment for later usage!!
-            let _increment = 1.0/signal_raw.len() as f64;
-
+            let _increment = 1.0/time_output.len() as f64;
             // best way found for the time domain
             // data in respect to the signal
-            let time: _ = linspace(0., 7., signal_raw.len())
+            let time: _ = linspace(0., 7., time_output.len())
                 .map_into::<f64>()
                 .collect_vec();
 
             // plot with gnuplot backend with the help of
             // gnuplot crate
-            let mut fg = Figure::new();
+            let plot_title =
+                if witch_channel == "Wind2" && self.state.find("c") == Some(0)
+            {"Compressed air"}
+            else{"Inverter"};
+            let mut fg = if *draw && channel.path == "Wind2" {Figure::new()} else {continue;};
             let the_title = format!("{} measurements", &plot_title);
             fg.set_title(&the_title)
               .axes2d()
               .set_x_label("Time (s)", &[])
               .set_y_label("Amplitute of signal", &[])
-              .lines(time, signal_raw,
-                     &[Caption      ("raw data"),
+              .lines(time, time_output,
+                     &[Caption      (&format!(" Inv {:1?} Ws {:2?}",
+                                              self.inv_state_exp,
+                                              self.ws)),
                        Color        ("#a705b0"),
                        LineWidth    (0.5)]);
-            // show the plot
-            fg.show().unwrap();
+
+            // check if user wants graph
+            if *draw {fg.show().unwrap();} else {break;}
+
+            // TODO attempt to save interactive semi-done!!
             let f_type: &str = ".png";
             let _save_to_file = format!(
                 "{}{}", plot_title, f_type)
@@ -153,7 +143,7 @@ pub fn plot_time_domain <'a> (file_name: TDMSFile, plot_title: &str) {
                 .replace("/","");
             // save to parent "project" folder
             // fg.save_to_png(save_to_file, 800, 600);
-        }
-        _i += 1;
-    }
+            // let big_title = &experiment.replace("c","C").replace("/", "");
+            // let big_title = &experiment.replace("i","I").replace("/", "");String::from("Wind2")
+        }}}
 }
