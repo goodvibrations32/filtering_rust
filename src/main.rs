ca21ea4d6191e2d8b58d51f51a6ffcb6ecc1842a
goodvibrations32 <goodvibrations32@protonmail.com>
2023-03-08 05:35:56 +0200

    make it more abstract!! wtf is happening with inverter folder

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,9 @@ use itertools_num::linspace;
 // use vector2d::Vector2D;
 use gnuplot::{Figure, Caption, Color,
               AxesCommon,LineWidth};
-/*
-use gnuplot::{PlotOption,LineWidth};
-use gnuplot::AlignType::AlignCenter;
-*/
 
 struct WindSupply {
-    compressed: String,
-    wind_tunnel: String,
+    type_of_experiment: String,
 }
 struct SysFolder {
     sys_path: String,
@@ -24,73 +19,31 @@ struct SysFolder {
     recording: String,
 }
 struct InnerExpFolders {
-    ca_inv_on_ws_0: String,
-    // ca_inv_on_ws_5: String,
-    // ca_inv_on_ws_10: String,
-    // ca_inv_off_ws_0: String,
-    // ca_inv_off_ws_5: String,
-    // ca_inv_off_ws_10: String,
-    // inv_inv_off_ws_0: String,
-    // inv_inv_on_ws_0: String,
-    // inv_inv_on_ws_5: String,
-    // inv_inv_on_ws_10: String,
-    // inv_inv_on_ws_15: String,
-    // inv_inv_on_ws_20: String,
-
+    ca: String,
+    inv: String,
 }
-// WindSupply::Compressed(String::from("compressed air"));
+
+// windsupply::Compressed(String::from("compressed air"));
 fn main(){
 
-    // wind supply method
-    let experiment = WindSupply {
-        compressed: String::from("compressed air/"),
-        wind_tunnel: String::from("inverter/"),
-    };
+    // be sure of what to plot
+    let system_path = String::from("/home/dtos_experiment/Documents/");
+    // FIXME goes out of bounds and plots 4 figures
+    // ONLY when using inverter's folder
+    let experiment = String::from("compressed air/");
+    // let experiment = String::from("inverter/");
+    let inverter_state = String::from("0");
+    let wind_speed = String::from("0");
 
-    // system folders such as pc folder, parent of data
-    // and recordings
-    let sys_folder = SysFolder {
-        sys_path: String::from("/home/dtos_experiment/Documents/"),
-        data_parent: String::from("data_folder/"),
-        recording: String::from("measurements_12_05_22/new_record_prop_channel/"),
-    };
-
-    //rename to better stuff but for now i have only one method experiment
-    let inverter_and_ws = InnerExpFolders {
-        ca_inv_on_ws_0: String::from("ca1_0.1/"),
-        // ca_inv_on_ws_5: String::from("ca1_5.1/"),
-        // ca_inv_on_ws_10: String::from("ca1_10.1/"),
-        // ca_inv_off_ws_0: String::from("ca0_0.1/"),
-        // ca_inv_off_ws_5: String::from("ca0_5.1/"),
-        // ca_inv_off_ws_10: String::from("ca0_10.1/"),
-
-        // inv_inv_off_ws_0: String::from("in0_0.1/"),
-        // inv_inv_on_ws_0: String::from("in1_0.1/"),
-        // inv_inv_on_ws_5: String::from("in1_5.1/"),
-        // inv_inv_on_ws_10: String::from("in1_10.1/"),
-        // inv_inv_on_ws_15: String::from("in1_15.1/"),
-        // inv_inv_on_ws_20: String::from("in1_20.1/"),
-    };
-    let local_place = sys_folder.sys_path;
-    let parent = sys_folder.data_parent;
-    let record = sys_folder.recording;
-    let experiment_type = experiment.compressed;
-    let experiment_state = inverter_and_ws.ca_inv_on_ws_0;
-    const FILE_NAME: &str = "Data.tdms";
-    let full_path = format!(
-        "{local_place}{parent}{record}{experiment_type}{experiment_state}{FILE_NAME}");
-    let path = Path::new(&full_path);
-    // if assert!(!path.try_exists().expect("no file BRO")) {
-    //     path = Path::new("D:/_data/WEL/WEL20220512/")
-
-    //     else {
-    //         path = path
-    //     }
-    // }
-    println!("{:?}", path);
+    // make the path
+    let sig_path = make_the_path(&system_path,
+                                 &experiment,
+                                 &inverter_state,
+                                 &wind_speed);
     // open a single file and store it in "file"
-    let file = match TDMSFile::from_path(
-        path
+    println!("{:?}", sig_path);
+    let sig = match TDMSFile::from_path(
+        Path::new(&sig_path)
     ) {
         // catch the error
         // dont know exactly
@@ -98,18 +51,63 @@ fn main(){
         Ok(f) =>f,
         Err(e) => panic!("{:?}", e),
     };
+    let big_title = &experiment.replace("c","C").replace("/", "");
+    // let big_title = &experiment.replace("i","I").replace("/", "");
+    plot_time_domain(sig, &big_title);
+}
 
-    // iterate in the file by the given group
-    // name from tdms object
-    let groups =file.groups();
+pub fn make_the_path (local: &str, exp_type: &str, inv: &str, ws: &str) -> String {
+    // wind supply method
+    let experiment = WindSupply {
+        type_of_experiment: String::from(exp_type),
+        // compressed: String::from("compressed air/"),
+        // wind_tunnel: String::from("inverter/"),
+    };
+
+    // system folders such as pc folder, parent of data
+    // and recordings
+    let sys_folder = SysFolder {
+        sys_path: String::from(local),
+        data_parent: String::from("data_folder/"),
+        recording: String::from("measurements_12_05_22/new_record_prop_channel/"),
+    };
+
+    //rename to better stuff but for now i have only one method experiment
+    let inverter_and_ws = InnerExpFolders {
+        ca: String::from(format!("ca{inv}_{ws}.1/")),
+        inv: String::from(format!("in0_0.1/")),
+    };
+
+    let local_place = sys_folder.sys_path;
+    let parent = sys_folder.data_parent;
+    let record = sys_folder.recording;
+    let experiment_type = experiment.type_of_experiment;
+    // let experiment_state =
+    //     if experiment_type == "compressed air/" {
+    //         let experiment_state = inverter_and_ws.ca;
+    //     }else {
+    //         let experiment_state = inverter_and_ws.inv;
+    //     };
+
+    let comp = inverter_and_ws.ca;
+    // FIXME
+    // let strng = inverter_and_ws.inv;
+    // FIXME
+    const FILE_NAME: &str = "Data.tdms";
+    let full_path = format!(
+        "{local_place}{parent}{record}{experiment_type}{comp}{FILE_NAME}");
+    full_path
+
+}
+
+pub fn plot_time_domain <'a> (file_name: TDMSFile, plot_title: &str) {
+    let groups =file_name.groups();
     for group in groups {
-        let channels = file.channels(&group);
-        // println!("chann-name {:?}", channels);
+        let channels = file_name.channels(&group);
         let mut _i = 0;
         for (_, channel) in channels{
-            // let mut data = file.channel_data_single_float(channel);
             let full_channel = match channel.data_type {
-                TdmsDataType::DoubleFloat(_) => file.channel_data_double_float(channel),
+                TdmsDataType::DoubleFloat(_) => file_name.channel_data_double_float(channel),
                 _ => {
                     panic!("{}", "channel for data type unimplemented")
                 }
@@ -137,7 +135,8 @@ fn main(){
             // plot with gnuplot backend with the help of
             // gnuplot crate
             let mut fg = Figure::new();
-            fg.set_title("Compressed air measurements")
+            let the_title = format!("{} measurements", &plot_title);
+            fg.set_title(&the_title)
               .axes2d()
               .set_x_label("Time (s)", &[])
               .set_y_label("Amplitute of signal", &[])
@@ -146,15 +145,15 @@ fn main(){
                        Color        ("#a705b0"),
                        LineWidth    (0.5)]);
             // show the plot
-            // fg.show().unwrap();
+            fg.show().unwrap();
             let f_type: &str = ".png";
             let _save_to_file = format!(
-                "{}{}", experiment_type, f_type)
+                "{}{}", plot_title, f_type)
                 .replace(" ", "_")
                 .replace("/","");
             // save to parent "project" folder
             // fg.save_to_png(save_to_file, 800, 600);
-            }
-            _i += 1;
         }
+        _i += 1;
+    }
 }
